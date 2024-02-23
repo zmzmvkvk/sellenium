fb8ea78ad8f1de2f80a69223966874629c15451d
unknown <zmzmvkvk@naver.com>
2024-02-23 17:32:16 +0900

    240223

--- app.js
+++ app.js
@@ -1,96 +1,59 @@
 require("dotenv").config();
+const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const Jimp = require("jimp");
 const axios = require("axios");
+const multer = require("multer");
+const csv = require("csv-parser");
 const { createWorker } = require("tesseract.js");
+const crawler = require("./routes/crawler");
+const { Builder, By, Capabilities } = require("selenium-webdriver");
 
-const getImageURL = require("./routes/crawler");
+// 파일 업로드를 위한 multer 미들웨어 설정
+const upload = multer({ dest: "uploads/" });
 
+app.use(express.static("public"));
 // JSON 파싱 미들웨어 등록
 app.use(bodyParser.json());
 
-app.get("/api/getImage", async (req, res) => {
-  try {
-    // 이미지 URL 가져오기
-    const imageURL = await getImageURL();
+let urlList = [];
 
-    // 이미지 URL을 클라이언트에 응답으로 보내기
-    res.send(imageURL);
-  } catch (error) {
-    console.error("에러:", error);
-    res.status(500).json({ error: "이미지 가져오기에 실패했습니다." });
-  }
+app.get("/", async (req, res) => {
+  res.sendFile("public/index.html");
 });
 
-// 이미지 OCR 및 번역
-app.post("/api/processImage", async (req, res) => {
-  try {
-    // 이미지 데이터 받아오기
-    const imageData = req.body.imageData;
+app.post("/readcsv", upload.single("csvFile"), async (req, res) => {
+  // CSV 파일을 읽고 데이터를 배열에 저장
+  const filePath = req.file.path;
+  const csv = fs.readFileSync(filePath, "utf-8");
+  urlList = csv.split(/\n|\r/); //CSV 파일 데이터의 배열
 
-    // OCR 워커 생성 및 초기화
-    const worker = createWorker();
-    await worker.load();
-    await worker.loadLanguage("chi_sim");
-    await worker.initialize("chi_sim");
+  // 사용자 에이전트 설정
+  const chromeCapabilities = Capabilities.chrome();
+  chromeCapabilities.set("chromeOptions", {
+    args: [
+      "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+      "--accept-language=ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+    ],
+  });
 
-    // 이미지에서 텍스트 추출
-    const {
-      data: { text },
-    } = await worker.recognize(imageData);
+  // 웹 드라이버 생성
+  let driver = new Builder()
+    .forBrowser("chrome")
+    .withCapabilities(chromeCapabilities)
+    .build();
 
-    // Naver Papago API 호출하여 번역
-    const response = await axios.post(
-      "https://openapi.naver.com/v1/papago/n2mt",
-      {
-        source: "zh-CN",
-        target: "ko",
-        text: text,
-      },
-      {
-        headers: {
-          "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-          "X-Naver-Client-Id": "VoXDKXzudUkyPZEDg0uF",
-          "X-Naver-Client-Secret": "0P3fl_JiWB",
-        },
-      }
-    );
+    await crawler.generate("https://smartstore.naver.com/richcommerce/products/9943371576", driver);
 
-    // 번역된 텍스트
-    const translatedText = response.data.message.result.translatedText;
+  // URL을 비동기적으로 처리
+  // for await (const url of urlList) {
+  //   await crawler.generate(url, driver);
+  // }
 
-    // 오버레이된 이미지 생성
-    const image = await Jimp.read(imageData);
-    const font = await Jimp.loadFont(Jimp.FONT_SANS_16_BLACK);
-    image.print(font, 10, 10, translatedText);
 
-    // 오버레이된 이미지를 base64로 변환하여 클라이언트에게 응답으로 보내기
-    const overlayedImage = await image.getBase64Async(Jimp.AUTO);
-    res.json({ overlayedImage });
-
-    // OCR 워커 종료
-    await worker.terminate();
-  } catch (error) {
-    console.error("이미지 처리 오류:", error);
-    res.status(500).json({ error: "이미지 처리에 실패했습니다." });
-  }
-});
-
-// 이미지 다운로드
-app.get("/api/downloadImage", async (req, res) => {
-  try {
-    // 클라이언트에게 전송할 이미지 데이터 받아오기
-    const imageData = req.body.imageData; // 이미지 데이터는 클라이언트에서 요청 본문으로 전송하거나 다른 방법으로 제공되어야 합니다.
-
-    // 이미지 데이터를 클라이언트에게 응답으로 보내기
-    res.setHeader("Content-Type", "image/jpeg"); // 이미지 타입에 따라 Content-Type을 설정합니다.
-    res.send(imageData);
-  } catch (error) {
-    console.error("이미지 다운로드 오류:", error);
-    res.status(500).json({ error: "이미지 다운로드에 실패했습니다." });
-  }
 });
 
 const PORT = process.env.PORT || 3000;
--- routes/crawler.js
+++ routes/crawler.js
@@ -1,142 +1,172 @@
-const { Builder, By, Capabilities } = require("selenium-webdriver");
+const { Builder, By, Capabilities, until } = require("selenium-webdriver");
+const fs = require("fs");
 
-// TODO: CSV에서 URL전달받은거 순회해서 url에 뿌려주기.
-const url = "https://smartstore.naver.com/richcommerce/products/9807129161";
+let crawlData = {
+  a: `__AUTO__`, //업체상품코드
+  b: ``, //모델명
+  c: ``, //브랜드
+  d: ``, //제조사
+  e: `해외=아시아=중국`, //원산지
+  f: ``, //상품명
+  g: ` `, //홍보문구
+  h: ``, //요약상품명
+  i: ``, //표준산업코드
+  j: ``, //카테고리코드
+  k: ``, //사용자분류명
+  l: ``, //한줄메모
+  m: ``, //시중가
+  n: ``, //원가
+  o: ``, //표준공급가
+  p: ``, //판매가
+  q: ``, //배송방법
+  r: ``, //배송비
+  s: ``, //과세여부
+  t: ``, //판매수량
+  u: ``, //이미지1URL
+  v: ``, //이미지2URL
+  w: ``, //이미지3URL
+  x: ``, //이미지4URL
+  y: ``, //GIF생성
+  z: ``, //이미지6URL
+  aa: ``, //이미지7URL
+  ab: ``, //이미지8URL
+  ac: ``, //이미지9URL
+  ad: ``, //이미지10URL
+  ae: ``, //추가정보
+  af: ``, //입력사항
+  ag: ``, //옵션구분
+  ah: ``, //선택옵션
+  ai: ``, //입력형옵션
+  aj: ``, //추가구매옵션
+  ak: ``, //상세설명
+  al: ``, //추가상세설명
+  am: ``, //광고/홍보
+  an: ``, //제조일자
+  ao: ``, //유효일자
+  ap: ``, //사은품내용
+  aq: ``, //키워드
+  ar: ``, //인증구분
+  as: ``, //인증정보
+  at: ``, //거래처
+  au: ``, //영어상품명
+  av: ``, //중국어상품명
+  aw: ``, //일본어상품명
+  ax: ``, //영어상세설명
+  ay: ``, //중국어상세설명
+  az: ``, //일본어상세설명
+  ba: ``, //상품무게
+  bb: ``, //영어키워드
+  bc: ``, //중국어키워드
+  bd: ``, //일본어키워드
+  be: ``, //생산지국가
+  bf: ``, //전세계배송코드
+  bg: ``, //사이즈
+  bh: ``, //포장방법
+  bi: ``, //개별카테고리
+  bj: ``, //상품상세코드
+  bk: ``, //상품상세1
+  bl: ``, //상품상세2
+  bm: ``, //상품상세3
+  bn: ``, //상품상세4
+  bo: ``, //상품상세5
+  bp: ``, //상품상세6
+  bq: ``, //상품상세7
+  br: ``, //상품상세8
+  bs: ``, //상품상세9
+  bt: ``, //상품상세10
+  bu: ``, //상품상세11
+  bv: ``, //상품상세12
+  bw: ``, //상품상세13
+  bx: ``, //상품상세14
+  by: ``, //상품상세15
+  bz: ``, //상품상세16
+  ca: ``, //상품상세17
+  cb: ``, //상품상세18
+  cc: ``, //상품상세19
+  cd: ``, //상품상세20
+  ce: ``, //상품상세21
+  cf: ``, //상품상세22
+  cg: ``, //상품상세23
+  ch: ``, //상품상세24
+  ci: ``, //상품상세25
+  cj: ``, //상품상세26
+};
 
-async function getImageURL() {
+async function generate(url, driver) {
   try {
-    // 사용자 에이전트 설정
-    const chromeCapabilities = Capabilities.chrome();
-    chromeCapabilities.set("chromeOptions", {
-      args: [
-        "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
-      ],
-    });
-
-    // 웹 드라이버 생성
-    let driver = await new Builder()
-      .forBrowser("chrome")
-      .withCapabilities(chromeCapabilities)
-      .build();
-
     // 웹 페이지 열기
     await driver.get(`${url}`);
-    // 이미지 요소 찾기
-    let imageElement = await driver.findElement(By.css(".bd_2DO68"));
 
-    const b = await driver.findElement(By.css("fieldset > div h3"));
+    // 상품명 가져오기
+    const b = await driver.wait(until.elementLocated(By.css("fieldset > div h3")), 10000);
     const bt = await b.getText();
+    const modelName = shuffleString(bt);
+    const brandName = "꾸러미배송 협력사";
+  
 
+    // 상품명 랜덤 재배치
     function shuffleString(targetText) {
       const text = targetText.split(" ");
       return [...text].sort(() => Math.random() - 0.5).join(" ");
     }
 
-    const brandName = `꾸러미배송 협력사`;
+    let categoriesList = [];
+    const categories = await driver.wait(
+      until.elementsLocated(By.css("._1_FPHJbv10")),
+      10000
+    );
 
-    const j = await driver.findElements(By.className("_1_FPHJbv10"));
+    for (const category of categories) {
+      categoriesList.push(await category.getText());
+    }
 
-    let crawlData = {
-      a: `__AUTO__`, //업체상품코드
-      b: `${shuffleString(bt)}`, //모델명
-      c: `${brandName}`, //브랜드
-      d: `${brandName}`, //제조사
-      e: `해외=아시아=중국`, //원산지
-      f: `${shuffleString(bt)}`, //상품명
-      g: ` `, //홍보문구
-      h: `${shuffleString(bt)}`, //요약상품명
-      i: `${j}`, //표준산업코드
-      j: ``, //카테고리코드
-      k: ``, //사용자분류명
-      l: ``, //한줄메모
-      m: ``, //시중가
-      n: ``, //원가
-      o: ``, //표준공급가
-      p: ``, //판매가
-      q: ``, //배송방법
-      r: ``, //배송비
-      s: ``, //과세여부
-      t: ``, //판매수량
-      u: ``, //이미지1URL
-      v: ``, //이미지2URL
-      w: ``, //이미지3URL
-      x: ``, //이미지4URL
-      y: ``, //GIF생성
-      z: ``, //이미지6URL
-      aa: ``, //이미지7URL
-      ab: ``, //이미지8URL
-      ac: ``, //이미지9URL
-      ad: ``, //이미지10URL
-      ae: ``, //추가정보
-      af: ``, //입력사항
-      ag: ``, //옵션구분
-      ah: ``, //선택옵션
-      ai: ``, //입력형옵션
-      aj: ``, //추가구매옵션
-      ak: ``, //상세설명
-      al: ``, //추가상세설명
-      am: ``, //광고/홍보
-      an: ``, //제조일자
-      ao: ``, //유효일자
-      ap: ``, //사은품내용
-      aq: ``, //키워드
-      ar: ``, //인증구분
-      as: ``, //인증정보
-      at: ``, //거래처
-      au: ``, //영어상품명
-      av: ``, //중국어상품명
-      aw: ``, //일본어상품명
-      ax: ``, //영어상세설명
-      ay: ``, //중국어상세설명
-      az: ``, //일본어상세설명
-      ba: ``, //상품무게
-      bb: ``, //영어키워드
-      bc: ``, //중국어키워드
-      bd: ``, //일본어키워드
-      be: ``, //생산지국가
-      bf: ``, //전세계배송코드
-      bg: ``, //사이즈
-      bh: ``, //포장방법
-      bi: ``, //개별카테고리
-      bj: ``, //상품상세코드
-      bk: ``, //상품상세1
-      bl: ``, //상품상세2
-      bm: ``, //상품상세3
-      bn: ``, //상품상세4
-      bo: ``, //상품상세5
-      bp: ``, //상품상세6
-      bq: ``, //상품상세7
-      br: ``, //상품상세8
-      bs: ``, //상품상세9
-      bt: ``, //상품상세10
-      bu: ``, //상품상세11
-      bv: ``, //상품상세12
-      bw: ``, //상품상세13
-      bx: ``, //상품상세14
-      by: ``, //상품상세15
-      bz: ``, //상품상세16
-      ca: ``, //상품상세17
-      cb: ``, //상품상세18
-      cc: ``, //상품상세19
-      cd: ``, //상품상세20
-      ce: ``, //상품상세21
-      cf: ``, //상품상세22
-      cg: ``, //상품상세23
-      ch: ``, //상품상세24
-      ci: ``, //상품상세25
-      cj: ``, //상품상세26
-    };
+      const categoryCodeExcel = fs.readFileSync(
+        "./excel/categories.csv",
+        "utf-8"
+      );
+      let categoryCodeList = categoryCodeExcel.split(/\n|\r/);
+      categoryCodeList.map((_, i) => {
+        if (categoryCodeList[i] == "") {
+          categoryCodeList.splice(i, 1);
+          i--;
+        }
+      });
+    categoryCodeList.map((category, i) => {
+      const splitedList = category.split(",");
+      splitedList.map((_, i) => {
+        if (_ === "") {
+          splitedList.splice(i, 1);
+          i--;
+        }
+      });
+      console.log(splitedList)
+    })
 
-    // 이미지 URL 가져오기
-    // let imageURL = await imageElement.getAttribute("src");
-    // 웹 드라이버 종료
-    // await driver.quit();
-
-    return crawlData;
+    // 데이터에 상품명 넣기
+    crawlData.b = modelName;
+    crawlData.c = brandName;
+    crawlData.d = brandName;
+    crawlData.f = modelName;
+    crawlData.h = modelName;
+    
   } catch (error) {
     console.error("에러:", error);
     throw new Error("이미지 가져오기에 실패했습니다.");
   }
 }
 
-module.exports = getImageURL;
+// async function getImageURL() {
+//   // 이미지 요소 찾기
+//   let imageElement = await driver.findElement(By.css(".bd_2DO68"));
+
+//   const brandName = `꾸러미배송 협력사`;
+
+//   
+
+//   // 이미지 URL 가져오기
+//   // let imageURL = await imageElement.getAttribute("src");
+//   // 웹 드라이버 종료
+//   // await driver.quit();
+// }
+
+module.exports = { generate };
